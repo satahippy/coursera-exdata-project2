ca1f6f7c31b270722215bbc4c296f53478a95734
satahippy <satahippy@gmail.com>
2015-11-22 18:04:04 +0300

    plot 5

---
+++ plot5.R
@@ -0,0 +1,31 @@
+setwd("/home/sata/tmp/3/r test/coursera/exdata/project3");
+library(stats)
+library(ggplot2)
+source("load-data.R")
+plotFile = "plot5.png"
+
+X11()
+
+# get data for plot
+# filter needed sources
+dataScc = SCC[grepl("vehicle", SCC$EI.Sector, ignore.case = T), 'SCC']
+# emissions for each year from vehicle sources in Baltimore City (fips == "24510")
+dataNei = aggregate(Emissions ~ year, NEI[NEI$fips == "24510" & NEI$SCC %in% dataScc, ], sum)
+dataNei$year = as.factor(dataNei$year)
+
+# draw plot
+qplot(
+    x = year,
+    y = Emissions,
+    data = dataNei,
+    geom = "bar",
+    stat = "identity",
+    position = "dodge",
+    main = expression('PM'[2.5]*' dynamic from motor vehicle sources in Baltimore City'),
+    xlab = "Year",
+    ylab = expression('PM'[2.5]*' emissions')
+)
+
+# save to file
+dev.copy(png, filename = plotFile)
+dev.off()
